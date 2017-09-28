d780d1cd84951cc4023dd525b48b375e94c6ee0d
Simon Lee <slee1994@hotmail.com>
2017-09-28 14:04:07 -0700

    Minor code changes
    Added a MaxDate and allignment fixes

--- 03-Age-Calculator/AgeCalc/AgeCalc/Form1.Designer.cs
+++ 03-Age-Calculator/AgeCalc/AgeCalc/Form1.Designer.cs
@@ -28,33 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(101, 156);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "To calculate your age in seconds, select your date of birth below:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Age in seconds:";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.Location = new System.Drawing.Point(149, 125);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(77, 13);
+            this.lblSeconds.TabIndex = 4;
+            this.lblSeconds.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(152, 157);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(89, 60);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 207);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(381, 192);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Age Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
--- 03-Age-Calculator/AgeCalc/AgeCalc/Form1.cs
+++ 03-Age-Calculator/AgeCalc/AgeCalc/Form1.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace AgeCalc
 {
+    //programming challenge 03 AgeCalculator  //@github/simonl94
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.MaxDate = DateTime.Today;
+        }
+
+        public void btnCalc_Click(object sender, EventArgs e)
+        {
+            lblSeconds.Text = ""+calcAge();
+        }
+
+        public double calcAge()
+        {
+            DateTime thisDay = DateTime.Now;
+            
+            return thisDay.Subtract(dateTimePicker1.Value).TotalSeconds;
+
         }
     }
 }
